4019898c719325059c0db6971fa17bbc84129319
Charles <charlesfleury19@hotmail.com>
2020-03-18 21:42:04 -0400

    Squelette programme + documentation

---
+++ src/controller/TranslateCommand.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TranslateCommand extends ImageCommand {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    public void translate(){
+
+    }
+}
---
+++ src/observer/MyObserver.java
@@ -0,0 +1,5 @@
+package observer;
+
+public interface MyObserver {
+    public void update();
+}
