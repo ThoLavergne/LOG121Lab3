c283f845f54016bde498be2051b7456231de7742
Charles <charlesfleury19@hotmail.com>
2020-04-01 14:50:36 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/controller/PerspectiveCommand.java
+++ src/controller/PerspectiveCommand.java
@@ -21,23 +21,14 @@ public abstract class PerspectiveCommand extends AbstractAction {
         this.p = p;
     }
 
-    //Cette méthode sert de méthode template, appelé losqu'un évènement écouté par une PerspectiveCommand arrive
+    //Cette méthode sert de méthode template, appelé losqu'un évènement écouté par une PerspectiveCommand se deroule
     public final void execute(){
 
         //On ajoute la commande a la liste de celles qu'on peut defaire
         CommandManager mngr = CommandManager.getInstance();
         mngr.addToUndo(this);
 
-        Memento backtrack = null;
-        if(this.undoMemento!= null){
-            backtrack = this.undoMemento.copy();
-        }
-
         this.undoMemento = p.getMemento();
-
-        if(backtrack != null){
-            this.undoMemento.setBacktrack(backtrack);
-        }
 
         //On execute l'action concrete
         executeAction();
@@ -48,26 +39,12 @@ public abstract class PerspectiveCommand extends AbstractAction {
      */
     public void undo(){
 
-        Memento backtrack = null;
-        if(this.redoMemento!=null){
-            backtrack = this.redoMemento.copy();
-        }
-
         //l'état actuel de la perspective est sauvée dans le redo memento
         this.redoMemento = p.getMemento();
 
-        //on sauvegarde le redomemento precedent dans le backtrack du redoMemento
-        if(backtrack!=null){
-            this.redoMemento.setBacktrack(backtrack);
-        }
 
         //la perspective est modifiée pour etre identique au courant undo memento
         p.setMemento(this.undoMemento);
-
-        //Le undo memento devient son memento precedent
-        if(undoMemento.getBacktrack()!=null){
-            this.undoMemento = this.undoMemento.getBacktrack();
-        }
 
     }
 
@@ -76,26 +53,11 @@ public abstract class PerspectiveCommand extends AbstractAction {
      */
     public void redo(){
 
-        Memento backtrack = null;
-        if(this.undoMemento!=null){
-            backtrack = this.undoMemento.copy();
-        }
-
         //l'état actuel de la perspective est sauvée dans le undo memento
         this.undoMemento = p.getMemento();
 
-        if(backtrack!=null){
-            this.undoMemento.setBacktrack(backtrack);
-        }
-        //on sauvegarde le redomemento precedent dans le backtrack du redoMemento
-
         //la perspective est modifiée pour etre identique au courant redo memento
         p.setMemento(this.redoMemento);
-
-        //Le redo memento devient son memento precedent
-        if(redoMemento.getBacktrack()!=null){
-            this.redoMemento = this.redoMemento.getBacktrack();
-        }
 
     }
 
--- src/memento/Memento.java
+++ src/memento/Memento.java
@@ -8,34 +8,18 @@ public class Memento {
 
     private double zoomMemorized;
     private Point translationMemorized;
-    //Chaque memento a son backtrack, un memento qui contient l'état qui le précédait
-    //Cela crée une liste chaînée de memento, qui permet de faire plusieurs "undos" successifs
-    private Memento backtrack;
 
     //Constructeurs
     public Memento(Perspective p){
         this.zoomMemorized = p.getZoom();
         this.translationMemorized = (Point) p.getTranslation().clone();
-        this.backtrack = null;
-    }
-
-    public Memento(Perspective p, Memento mem){
-        this.zoomMemorized = p.getZoom();
-        this.translationMemorized = (Point) p.getTranslation().clone();
-        this.backtrack = mem.copy();
     }
 
     public Memento(double zoom, Point translation){
         this.zoomMemorized = zoom;
         this.translationMemorized = (Point) translation.clone();
-        this.backtrack = null;
     }
 
-    public Memento(double zoom, Point translation, Memento mem){
-        this.zoomMemorized = zoom;
-        this.translationMemorized = (Point) translation.clone();
-        this.backtrack = mem.copy();
-    }
 
     //Accesseurs
     public double getZoomMemorized() {
@@ -46,27 +30,10 @@ public class Memento {
         return (Point) translationMemorized.clone();
     }
 
-    public Memento getBacktrack(){
-        if(this.backtrack!=null){
-            return this.backtrack.copy();
-        }
-        else{
-            return null;
-        }
-    }
 
     //Methode retournant une copie du memento
     public Memento copy(){
-        if(this.backtrack != null){
-            return new Memento(this.zoomMemorized, (Point) this.translationMemorized.clone(), this.backtrack.copy());
-        }
-        else{
-            return new Memento(this.zoomMemorized, (Point) this.translationMemorized.clone());
-        }
+        return new Memento(this.zoomMemorized, (Point) this.translationMemorized.clone());
     }
 
-    //Methode qui pose le backtrack de ce memento
-    public void setBacktrack(Memento backtrack){
-        this.backtrack = backtrack.copy();
-    }
 }
